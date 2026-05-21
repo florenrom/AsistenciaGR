88d1cb07b188b84e964b3e7dd6dd5069c86961e0
crism03 <cristianmeza440@gmail.com>
2026-05-21 11:37:17 -0300

    agragado los datos del modelo Asistencia

---
+++ Models/Asistencia.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsistenciaGR.Models
+{
+    public class Asistencia
+    {
+        // Identificador único del registro.
+        [Key]
+        public int AsId { get; set; }
+
+        // Fecha y hora de la toma de asistencia.
+        public DateTime AsFechaHora { get; set; }
+
+        // Bloque horario de la clase impartida.
+        public string AsModulo { get; set; }
+
+        // Motivo de ausencia.
+        public string? AsJustificacion { get; set; }
+
+        // Asignatura correspondiente al registro.
+        public string AsMateria { get; set; }
+
+        // Programa de estudio o carrera.
+        public string AsCarrera { get; set; }
+
+        // Profesor que dicta la clase.
+        public string AsDocente { get; set; }
+
+        // Clave foránea que conecta con el estudiante.
+        // public int UsId { get; set; } por el momento no se utiliza
+
+        // conexion hacia el modelo Usuarios.
+        // public virtual Usuarios? Usuario { get; set; }
+    }
+}
