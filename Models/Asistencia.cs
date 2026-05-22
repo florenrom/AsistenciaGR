3caf6112d9d019694d507c500c45fe8652d567b0
crism03 <cristianmeza440@gmail.com>
2026-05-22 12:01:24 -0300

    Datos en el modelo Asistencias arreglado

--- Models/Asistencia.cs
+++ Models/Asistencia.cs
@@ -1,35 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AsistenciaGR.Models
 {
+
+    // Representa el modelo de datos para registrar la asistencia a una clase. 
     public class Asistencia
     {
         // Identificador único del registro.
-        [Key]
         public int AsId { get; set; }
-
-        // Fecha y hora de la toma de asistencia.
+        // Fecha y hora exacta en la toma de asistencia.
         public DateTime AsFechaHora { get; set; }
-
-        // Bloque horario de la clase impartida.
+        //Bloque horario de la clase.
         public string AsModulo { get; set; }
-
-        // Motivo de ausencia.
+        // Motivo o justificación en caso de ausencia.
         public string? AsJustificacion { get; set; }
-
-        // Asignatura correspondiente al registro.
+        // Nombre de la materia correspondiente al registro.
         public string AsMateria { get; set; }
-
-        // Programa de estudio o carrera.
+        // Nombre del programa de la carrera.
         public string AsCarrera { get; set; }
-
-        // Profesor que dicta la clase.
+        // Nombre del docente a cargo de dictar la clase.
         public string AsDocente { get; set; }
 
         // Clave foránea que conecta con el estudiante.
-        // public int UsId { get; set; } por el momento no se utiliza
+        // public int UsId { get; set; } // Por el momento no se utiliza
 
-        // conexion hacia el modelo Usuarios.
-        // public virtual Usuarios? Usuario { get; set; }
+        // Conexión hacia el modelo Usuarios.
+        // public virtual Usuarios? Usuario { get; set; } // Por el momento no se utiliza
     }
 }
